e3feca5522bb30f388be8df74e0d1d11e82ec5ad
Chris Eidhof <chris@eidhof.nl>
2015-04-03 14:59:47 +0200

    Cleanup

---
+++ CoreImageVideo/StaticVideoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StaticVideoViewController.swift
+//  CoreImageVideo
+//
+//  Created by Chris Eidhof on 03/04/15.
+//  Copyright (c) 2015 objc.io. All rights reserved.
+//
+
+import UIKit
+
+class StaticVideoViewController: UIViewController {
+    
+}
