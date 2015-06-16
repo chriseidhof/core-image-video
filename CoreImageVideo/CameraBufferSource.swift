5784ae991c1c71efb30ad9444d8cb27c84dab008
adam zethraeus <adam@sparkslabs.co>
2015-06-16 13:50:08 -0700

    Removed private visibility of CaptureBufferDelegate
    * This makes SimpleFilterViewController render video

--- CoreImageVideo/CameraBufferSource.swift
+++ CoreImageVideo/CameraBufferSource.swift
@@ -51,7 +51,7 @@ struct CaptureBufferSource {
     }
 }
 
-private class CaptureBufferDelegate: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
+class CaptureBufferDelegate: NSObject, AVCaptureVideoDataOutputSampleBufferDelegate {
     let callback: CMSampleBuffer -> ()
     
     init(_ callback: CMSampleBuffer -> ()) {
